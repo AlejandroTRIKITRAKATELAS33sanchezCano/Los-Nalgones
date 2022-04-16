9afd98d7d848ca7a5c138b50d7fbc03e51d9b64c
Alejandro Trikitrakatelas Sanchez Cano <99841453+AlejandroTRIKITRAKATELAS33sanchezCano@users.noreply.github.com>
2022-04-16 00:50:23 -0500

    HTML

--- HTML/CSS/Js/Seleccion.js
+++ HTML/CSS/Js/Seleccion.js
@@ -17,3 +17,24 @@ select.addEventListener('click', () => {
 	select.classList.toggle('active');
 	opciones.classList.toggle('active');
 });
+
+
+const seleccionar = document.querySelector('#selecionar');
+const options = document.querySelector('#options');
+const contenido = document.querySelector('#selecionar .contenido-seleccionar');
+const hidden = document.querySelector('#inputSeleccionar');
+
+document.querySelectorAll('#options > .option').forEach((opcion) => {
+	opcion.addEventListener('click', (e) => {
+		e.preventDefault();
+		contenido.innerHTML = e.currentTarget.innerHTML;
+		seleccionar.classList.toggle('active');
+		options.classList.toggle('active');
+		hidden.value = e.currentTarget.querySelector('.titulo').innerText;
+	});
+});
+
+seleccionar.addEventListener('click', () => {
+	seleccionar.classList.toggle('active');
+	options.classList.toggle('active');
+});
