49d4b70bc2e7d7347555be51998a8eb39a79df7e
Daniel89662 <101789012+Daniel89662@users.noreply.github.com>
2022-04-18 19:32:27 -0500

    Delete 15Examen directory

--- 15Examen/web/Js/Registrar.js
+++
@@ -1,9 +0,0 @@
-function Grupo() {
-
-    var s = document.getElementById('semestre').value;
-    var t = document.getElementById('Turno').value;
-    var g = document.getElementById('Grupo').value;
-
-    alert(s+"I"+t+g)
-    
-}
