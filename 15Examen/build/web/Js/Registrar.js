3c2e4386c908da791fb191efe97e4ef60ad2e89c
Daniel89662 <101789012+Daniel89662@users.noreply.github.com>
2022-04-18 23:12:59 -0500

    Add files via upload

---
+++ 15Examen/build/web/Js/Registrar.js
@@ -0,0 +1,9 @@
+function Grupo() {
+
+    var s = document.getElementById('semestre').value;
+    var t = document.getElementById('Turno').value;
+    var g = document.getElementById('Grupo').value;
+
+    alert(s+"I"+t+g)
+    
+}
